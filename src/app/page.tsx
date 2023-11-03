a0e2635473f4391f00033188e2e7042650653d89
Oliullah <oliullahservice@gmail.com>
2023-11-03 16:54:37 +0600

    problem fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 'use client'
 
-import Image from 'next/image'
-
-
 import Carossel from "@/components/Carossel";
-import Featured from "@/components/Featured";
-import Offer from "@/components/Offer";
-import Slider from "@/components/Slider";
-import Cart from '@/components/Aboutus';
 import Aboutus from '@/components/Aboutus';
 
 export default function Home() {
